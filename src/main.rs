df612411ae58a3b7acc95e5f4c9cf8e0251c9900
sivaprakashkrp <sivaprakashkrp@gmail.com>
2026-03-19 23:46:14 +0530

    Created input boxes and tested integration of fonts

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
 use eframe::egui::{
-    self, CentralPanel, ComboBox, Context, FontFamily, FontId, ScrollArea, TextStyle, TopBottomPanel, Ui
+    self, CentralPanel, Context, FontFamily, FontId, ScrollArea, TextStyle, TopBottomPanel, Ui
 };
 
 #[derive(Default)]
 struct App {
-
+    input_eqn: String,
+    
 }
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
-        todo!("Render input field, display field and graphs");
+        set_styles(ctx);
+        show_top_bar(ctx);
+        CentralPanel::default().show(ctx, |ui| {
+            self.show_expr_input(ui);
+        });
+    }
+}
+
+impl App {
+    fn show_expr_input(&mut self, ui: &mut Ui) {
+        ui.vertical_centered_justified(|ui| {
+            ui.horizontal(|ui| {
+                ui.label("Input Expression: ");
+                ui.add_sized(
+                    egui::vec2(500.0, 100.0), 
+                    egui::TextEdit::singleline(&mut self.input_eqn)       
+                );
+            });
+            ui.horizontal(|ui| {
+                if ui.button("Apply FFT").clicked() {
+                    // todo!("Apply FFT");
+                }
+                if ui.button("Clear").clicked() {
+                    self.input_eqn.clear();
+                }
+            })
+        });
     }
 }
 
@@ -21,5 +48,31 @@ fn main() {
         ..Default::default()
     };
     let _ = eframe::run_native("VisualFFT", options, Box::new(|_cc| Ok(Box::<App>::default())));
-    println!("App Closed");
+}
+
+fn set_styles(ctx: &Context) {
+    let mut style = (*ctx.style()).clone();
+    style.text_styles = [
+        (TextStyle::Heading, FontId::new(25.0, FontFamily::Monospace)),
+        (TextStyle::Body, FontId::new(16.0, FontFamily::Monospace)),
+        (TextStyle::Button, FontId::new(16.0, FontFamily::Monospace)),
+        (TextStyle::Small, FontId::new(14.0, FontFamily::Monospace)),
+    ]
+    .into();
+    ctx.set_style(style);
+}
+
+fn show_top_bar(ctx: &Context) {
+    TopBottomPanel::top("menu_bar").show(ctx, |ui| {
+        egui::menu::bar(ui, |ui| {
+            ui.menu_button("File", |ui| {
+                if ui.button("About").clicked() {
+                    // todo!("Render About Dialog Box")
+                }
+                if ui.button("Exit").clicked() {
+                    ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                }
+            })
+        })
+    });
 }
